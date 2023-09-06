bf4fed64a53ccaa20cd1facb492780b8881a1751
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-07 01:08:39 +0530

    basic

---
+++ basic_3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name; //here name is a string data type .
+    cout << "Let me know your name ";
+    cin >> name;
+    cout << "Welcome!Mr./Mrs. " << name;
+    return 0;
+}
