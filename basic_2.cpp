f4563786cff35253be665c77373d0948ec4ba309
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-07 01:02:18 +0530

    basic

---
+++ basic_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b, c;
+    cout << "Please give the input of number";
+    cin >> a >> b;
+    c = a * b;
+    cout << "The multiplication is" << c;
+    return 0;
+}
