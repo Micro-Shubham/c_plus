5b2c936e9e456bcf46034ea77709ab529433b1fd
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-19 18:01:15 +0530

    basic

---
+++ basic_8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float area, r;
+    cout << "Value of radius ";
+    cin >> r;
+    area = 3.1425f * r * r;
+    cout << "The area of circle is " << area;
+    return 0;
+}
