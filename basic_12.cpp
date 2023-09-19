12c34df30a03cd8bb2d41a2120cc76d8249c58f7
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-19 21:33:25 +0530

    bitwise operator

---
+++ basic_12.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 11, y = 5, z; // bitwise operator
+    z = x & y;            // here and operator is used .
+    cout << z;
+    return 0;
+}
