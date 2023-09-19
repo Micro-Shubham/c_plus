cedb7dcdc97c9e5fe80953fe0bfd66faf13e62a9
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-19 22:14:56 +0530

    enum data type

---
+++ basic_13.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+enum day
+{
+    mon,
+    tue,
+    wed,
+    thru,
+    fri,
+    sat,
+    sun
+};         // enum is a user define data type .
+int main() // mon has value 0 and rest are in increament way.
+{
+    day d; // here day is a data type .
+    d=mon;
+    cout <<tue;
+    return 0;
+}
