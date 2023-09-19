f29790622a649a3b6b23f0ad4eeca721c0c3f268
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-19 22:43:49 +0530

    typedef

---
+++ basic_15.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+typedef int marks; // this is a concept of typedef.
+int main()
+{
+
+    marks m1, m2, m3;
+    m1 = 3;
+    m2 = 4;
+    cout << m1<<"  " << m2 << endl;
+    return 0;
+}
