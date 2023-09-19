f9a6516542a2f2b977c918df5a856a883c6f3bb2
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-19 19:49:57 +0530

    overflow

---
+++ basic_11.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <climits> //standered library to access INT_MAX
+using namespace std;
+int main()
+{
+    int n = INT_MAX; // assign the maximum value to varible.
+    n++;
+    cout << n;
+    return 0;
+}
