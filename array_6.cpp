f058c73d40f34363349fb895ea2b256a867a81cb
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-23 16:22:11 +0530

    array

---
+++ array_6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A[] = {1, 5, 9, 8, 0};
+    for (int i = 0; i < 5; i++)
+    {
+        cout << A[i] << endl;
+    }
+    return 0;
+}
