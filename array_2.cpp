7e949ab9f281776db2aa3cfe095b6291fd97d512
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-23 15:48:52 +0530

    array

---
+++ array_2.cpp
@@ -0,0 +1,12 @@
+// Printing whole array
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A[6] = {2, 5, 6, 1, 5, 9};
+    for (int i = 1; i < 6; i++)
+    {
+        cout << A[i] << endl;
+    }
+    return 0;
+}
