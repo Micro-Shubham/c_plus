f06369133ba916cc265f07048b132ad039b02a1b
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-23 15:44:00 +0530

    basic understanding of array

---
+++ array_1.cpp
@@ -0,0 +1,21 @@
+//Scaler x=5;
+/*
+Vector array=array which have magnitude as well as dimension .
+like A=(5,5,6,2,1,0) array having magnitude and have a one dimension .
+How to assign array ?
+int A[5]={5,2,8,1,2} here array of size 5 so it will contain 5 element of same data type .
+each element have index value .The index value start from 0 . In this array the index value is from 0 to 4. Index value
+is also know as subscripts.
+
+How to access any arrry element ?
+cout<<A[3]; this will print that element which have index value 3.
+
+
+
+
+
+
+
+
+
+*/
