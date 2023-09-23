23c3c54345dcb1c2f5bdf0a7db6f0a95869e6b43
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-23 19:12:55 +0530

    linear search

---
+++ array_11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int key, i, j, A[5];
+    cout << "Enter the element of an array" << endl;
+    for (i = 0; i < 5; i++)
+    {
+        cin >> A[i];
+    }
+    cout << "Enter the key to be search" << endl;
+    cin >> key;
+    for (j = 0; j < 5; j++)
+    {
+        if (key == A[j])
+        {
+            cout << "The searching key index number is:" << j << endl;
+        }
+    }
+    return 0;
+}
