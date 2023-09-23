1d62b139980c96a73d0bc90a3448202a674677b0
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-23 16:10:15 +0530

    array

---
+++ array_4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char A[5] = {'a','b','c','d','e'};
+    for (int i = 0; i < 5; i++)
+    {
+        cout << A[i] << endl;
+    }
+    return 0;
+}
