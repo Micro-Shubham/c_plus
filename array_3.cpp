0ec0fa7e3eafcdba2be423f847df822b88214158
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-23 16:02:58 +0530

    array

---
+++ array_3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float A[5] = {1.2, 3.2, 4.2, 1.4, 1.6};
+    for (int i = 0; i < 5; i++)
+    {
+        cout << A[i] << endl;
+    }
+    return 0;
+}
