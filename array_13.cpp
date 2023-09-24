52d45c3c7f6fc9eb89da87f9ce539dfeb8466233
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-24 16:49:07 +0530

    nested for loop

--- array_13.cpp
+++ array_13.cpp
@@ -5,7 +5,8 @@ int main()
     for (int i = 1; i < 5; i++)
     {
         for (int j = 1; j < 5; j++)
-            cout << j;
+            cout << "(" << i << "," << j << ")"
+                 << "  ";
         cout << endl;
     }
 
