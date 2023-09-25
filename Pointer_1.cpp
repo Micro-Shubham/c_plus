9a48ddc478a293bf4e72a7e64efe5ce3a5411f26
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-26 01:46:35 +0530

    basic

---
+++ Pointer_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 5;         // data variable.
+    int *p;            // address variable.
+    p = &x;            // p will store the address of variable x in p.
+    cout << p << endl; // printing the p which give the address of variable x.And this p will also take some occupy some space.
+
+    cout << x; // printing the value of x.
+    return 0;
+}
+// Now pointer is a variable which is used for  storing  the address of data .
