68d18640320c7252855dffa5e43d34c22f32ea8f
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-26 01:59:50 +0530

    pointer

--- Pointer_1.cpp
+++ Pointer_1.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main()
 {
     int x = 5;         // data variable.
-    int *p;            // address variable.
-    p = &x;            // p will store the address of variable x in p.
+    int *p;            // address variable.To pointer is declared by using star(*).
+    p = &x;            // p will store the address of variable x in p.And(&) sign is used with the variable to initialize the address of that variable.
     cout << p << endl; // printing the p which give the address of variable x.And this p will also take some occupy some space.
 
     cout << x << endl;  // printing the value of x.
