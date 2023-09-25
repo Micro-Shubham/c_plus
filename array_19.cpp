10d083db53b6b5e35b1adfe48f83ffd4e7061399
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-26 01:08:45 +0530

    multiplication of matrix

--- array_19.cpp
+++ array_19.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int A[2][2], B[2][2], C[2][2];
     cout << "Enter the element of matrix A:\n";
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < 2; i++) // taking input for matrix A.
     {
         for (int j = 0; j < 2; j++)
         {
@@ -12,7 +12,7 @@ int main()
         }
     }
     cout << "Enter the element of matrix B:\n";
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < 2; i++) // taking input for matrix B.
     {
         for (int j = 0; j < 2; j++)
         {
@@ -20,14 +20,14 @@ int main()
         }
     }
     cout << "Maltiplication of matrix A and B" << endl;
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < 2; i++) // multiplying matrix A and B.Also assigning its value in matrix C.
     {
         for (int j = 0; j < 2; j++)
         {
-            C[i][j] = A[i][j] + B[i][j];
+            C[i][j] = A[i][j] * B[i][j];
         }
     }
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < 2; i++) // printing matrix C.
     {
         for (int j = 0; j < 2; j++)
         {
