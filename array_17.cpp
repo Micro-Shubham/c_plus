b32de85ad88770ca70cf62030f9d849774101434
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-26 00:00:22 +0530

    2d array

---
+++ array_17.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A[2][3];
+    for(int i=1;i<=6;i++)
+    {
+        cout<<A[2][3];
+        cout<<endl;
+    }
+    
+    return 0;
+}
