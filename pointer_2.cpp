7dd541317806ad1ec931d3c23502c0373b04d325
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-26 18:57:08 +0530

    purpose of pointer

---
+++ pointer_2.cpp
@@ -0,0 +1,24 @@
+/*
+What is the purpose of pointer ?
+Ans- A program can access stack but not heap memory .So for this to access heap memory pointer is use .
+Program acess pointer and pointer access heap memory .Hence , a program can access heap memory only with
+the help of pointer indirectly.
+Second purpose:
+Suppose a file is there in a disk and we have to access it but program can access indirectly only with the help
+of pointer .Without taking help of pointer accessing that file for a program is difficult.We use file pointer to
+access that file using pointer.
+Third purpose:
+A program can access network connection indirectly with help of pointer.
+Forth purpose:
+A program can access keyboard, mouse,printer etc indirectly with the help of pointer.
+Pointer is available in C/C++.
+Where as in java and C# they cannot do it because there is not the concept of pointer.
+For access we have to use JVM or common language of run time in C# or dot net.
+
+
+Pointer is only available in C/C++ .
+By using C/C++ we can do system programming , we can develop even operating system , we can develop
+the device drivers only the reason we have is pointer .
+
+
+*/
