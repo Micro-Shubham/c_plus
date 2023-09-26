dbdc0e0c0fcd4c1176046601bb52b6ed98b705e9
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-26 23:14:17 +0530

    pointer arithmatci

--- pointer_9.cpp
+++ pointer_9.cpp
@@ -7,7 +7,7 @@ int main()
     for (int i = 0; i < 3; i++)
     {
         cout << *p << endl;
-        p = p + 2;
+        p = p + 2; // this will move two step forward .p store the location of array A.
     }
     return 0;
 }
