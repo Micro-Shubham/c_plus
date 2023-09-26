3bacdc3135fab6d9b4c1c47d1395c1f488c3fca9
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-26 23:16:34 +0530

    pointer arithmatic

--- pointer_9.cpp
+++ pointer_9.cpp
@@ -4,10 +4,8 @@ int main()
 {
     int A[5] = {5, 4, 5, 6, 7};
     int *p = A;
-    for (int i = 0; i < 3; i++)
-    {
-        cout << *p << endl;
-        p = p + 2; // this will move two step forward .p store the location of array A.
-    }
+    p = p + 2;
+    cout << p << "  " << *p;
+
     return 0;
 }
