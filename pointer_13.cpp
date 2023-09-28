6bbabc4becdc65553611fea7bf1851031e34d9b9
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-09-28 23:24:21 +0530

    funtion pointer call

--- pointer_13.cpp
+++ pointer_13.cpp
@@ -6,6 +6,9 @@ void display()
 }
 int main()
 {
-    display();
+    void (*funtion_pointer)(); // declare funtion pointer to take no arrgument and return void.
+    funtion_pointer = display; // assign address of the display funtion to the pointer.
+    (*funtion_pointer)();      // call the funtion through the pointer.
+
     return 0;
 }
