51a5f53c3f70f0a4494245a6a9f2c7615771e688
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-03 23:35:03 +0530

    substring

--- string_11.cpp
+++ string_11.cpp
@@ -4,8 +4,12 @@ using namespace std;
 int main()
 {
     char s1[20] = "pragramming";
-    char s2[20] = "gram";
-    cout << strstr(s1, s2); // substring
+    char s2[20] = "b";
+    if (strstr(s1, s2) != NULL)
+        cout << strstr(s1, s2); // substring
+
     // substring will find the enter string and print the remaining string.
+    else
+        cout << "not found";
     return 0;
 }
