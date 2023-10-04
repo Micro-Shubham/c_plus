f05ab641e8746d47163f5d09a172238069cf83a0
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-05 01:09:27 +0530

    str.empty() funtion of class string

--- string_25.cpp
+++ string_25.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main()
 {
     string str = "";
-    cout << str.empty(); // check wheather the string is empty or not. For true 1 and false 0.
+    cout << str.empty(); // check whether the string is empty or not. For true 1 and false 0.
     return 0;
 }
