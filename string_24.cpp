8c98c04084b3eada4ec6d248856e8a72ab41925a
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-05 01:02:54 +0530

    str.clear() funtion of class string

---
+++ string_24.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str = "hello ";
+    str.clear(); // clear the class string.
+    cout << str.length();
+    return 0;
+}
