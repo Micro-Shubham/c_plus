9ceebdbafc7f49b6fc8d10528bd13e9ba7682433
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-05 00:42:40 +0530

    str.resize()funtion of class string

---
+++ string_22.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str;
+    cout << "Enter a string:";
+    getline(cin, str);
+    cout << str.capacity() << endl;
+    str.resize(20); // resize the capacity of class string .
+    cout << str.capacity();
+    return 0;
+}
