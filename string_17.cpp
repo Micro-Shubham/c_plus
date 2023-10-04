5ecbef5b6741f388ba80e99f693976d4baa1f0a8
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-04 23:22:06 +0530

    string class

--- string_17.cpp
+++ string_17.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     // declaration and initialisation
-    string a = "Hello";
+    string a = "Hello"; // string class help to create the an internal array of bigger size then the required space.
     cout << a;
     return 0;
 }
