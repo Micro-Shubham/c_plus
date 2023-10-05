4124b3f25bf408e1631b19a095a34b46356093c7
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-05 23:43:21 +0530

    str.at() funtion of class string

---
+++ string_40.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str = "Hello";
+    cout << str.at(0); // output will be the letter at the entered index value.
+    return 0;
+}
