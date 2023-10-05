47bea5433956e70252c23810e65a54fa6a81f527
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-06 00:06:18 +0530

    equal(=) class operator of class stirn

---
+++ string_44.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s1 = "Hello";
+    string s2;
+    s2 = s1; // string of first will copy in s2.
+    cout << s2;
+    return 0;
+}
