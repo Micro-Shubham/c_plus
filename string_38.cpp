a010b4d92aef01801f13ce631ad8675ce24c80c4
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-05 23:28:04 +0530

    str.substr(start,number)

--- string_38.cpp
+++ string_38.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     string str = "Programming";
-    cout << str.substr(3,10); // str.substr will give the output from enter index to upto entered index.
-    // object.substr(start,end). 
+    cout << str.substr(3,4); // str.substr will give the output from enter index to upto entered index.
+    // object.substr(start,number). 
     return 0;
 }
