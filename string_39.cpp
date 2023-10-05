e8708082e0c9409e96fcfd092f81b8556db4bdb6
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-05 23:24:14 +0530

    s1.compare(s2) funtion of class string

---
+++ string_39.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s1 = "Zebra";
+    string s2 = "World";
+    cout << s1.compare(s2); // compare two string by dictionary.
+    return 0;
+}
