24c150a3cb00e33deb023edb54bb493412e8765d
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-05 13:52:16 +0530

    str.append() funtion of class string

--- string_26.cpp
+++ string_26.cpp
@@ -6,7 +6,9 @@ int main()
     string str;
     cout << "Enter a string:";
     getline(cin, str);
+    cout << str.capacity() << endl;
     str.append(" bye"); // add this word last of the entered string.
     cout << str << endl;
+    cout << str.capacity() << endl;
     return 0;
 }
