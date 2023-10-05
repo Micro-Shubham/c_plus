40fbcf3b62e1c94c4898abd1f3fbdc50cd7d73a5
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-05 16:38:51 +0530

    str.find() funtion of class string

---
+++ string_34.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str = "How are you";
+    cout << str.find("are"); // find the index value of entered string.If enterd string is not there it will print the maximum index value.
+    return 0;
+}
