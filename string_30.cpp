62cbd8a2441906592508dc802029ef45b099b7cd
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-05 15:42:49 +0530

    str.push_back('') funtion of class string

---
+++ string_30.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str = "hell";
+    str.push_back('o'); // insert a o at the end of the string .
+    cout << str;
+    return 0;
+}
