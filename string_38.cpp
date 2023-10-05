d4e73fb42afc4480309bfbb0e34627d4d565b632
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-05 23:17:05 +0530

    str.substr(start,end)

---
+++ string_38.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string >
+using namespace std;
+int main()
+{
+    string str = "Programming";
+    cout << str.substr(3, 10); // str.substr will give the output from enter index to upto entered index.
+    // object.substr(start,end).
+    return 0;
+}
