2f4a45ed90bd5d9f14c6c84a5c85d92e98436450
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-05 15:49:20 +0530

    str.swap(s) funtion of class

---
+++ string_31.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str = "apple";
+    string s = "fruit";
+    str. swap(s);
+
+    cout << str;
+    return 0;
+}
