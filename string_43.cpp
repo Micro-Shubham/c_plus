6f640b1cbf1be040d59588a02af289f003b5f437
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-06 00:01:13 +0530

    s1+s2 class operator of class string

---
+++ string_43.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s1 = "Hello";
+    string s2 = " World";
+    cout << s1 + s2; // concatinate(add) two string.
+    return 0;
+}
