76f2396892b10f361406b4731edc3133105da410
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-06 14:50:34 +0530

    basic

---
+++ string_48.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str = "Programming";
+    for (int i = 0; i < 11; i++)
+    {
+        cout << str[i] << endl;
+    }
+    return 0;
+}
