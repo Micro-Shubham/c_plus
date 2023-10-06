a50c64dc61437336a5ce74df8ae72b2dc0621989
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-06 14:53:45 +0530

    basic string

---
+++ string_49.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str = "Pragramming";
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        cout << str[i] << endl;
+    }
+    return 0;
+}
