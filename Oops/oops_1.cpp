f328a59a0d379adf56c334ddb6ff06013682be6b
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-09 20:00:35 +0530

    object and class

---
+++ Oops/oops_1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class rectangle
+{
+    // public
+public:
+    int length, breadth;
+    // function member
+    int area()
+    {
+        return length * breadth;
+    }
+    // function member
+    int perimeter()
+    {
+        return 2 * (length + breadth);
+    }
+};
+int main()
+{
+    // class is rectangle and object is r1 and r2
+    rectangle r1, r2;
+    // access length and breadth
+    r1.length = 12;
+    r1.breadth = 23;
+    cout << r1.area() << endl;
+    cout << r1.perimeter() << endl;
+
+    return 0;
+}
