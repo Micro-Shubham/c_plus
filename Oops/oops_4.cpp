9d7774bb36011f9aba7cae22656c05c8ff9006ab
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-10 14:29:01 +0530

    class as pointer

---
+++ Oops/oops_4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class rectangle
+{
+public:
+    int length;
+    int breadth;
+    int area()
+    {
+        return length * breadth;
+    }
+    int perimeter()
+    {
+        return 2 * (length + breadth);
+    }
+};
+int main()
+{
+    // rectanle class is now a stored in heap memory
+    rectangle *p = new rectangle();
+    // p stored in stack and it will declaring the value of length and breadth value which is stored in heap memory
+    p->length = 10;
+    p->breadth = 20;
+    cout << "Area is " << p->area();
+}
