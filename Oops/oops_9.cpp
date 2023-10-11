784b55b2c1b8bd5e2bcb878d17074cf4cf886440
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-11 22:23:44 +0530

    scope resolution in class

--- Oops/oops_9.cpp
+++ Oops/oops_9.cpp
@@ -1,30 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class rectangle
 {
- private:
- int length;
- int breadth;
- public:
- //constructors
- rectangle();
- rectangle(int l,int b);
- rectangle(rectangle &r);
- //mutator
- int getlength();
- int getbreadth();
- //accessor
- void setlength(int l);
- void setbreadth(int b);
- //facilatator
- int area();
- int perimeter();
- //enquary
- bool isSquare();
- //destructor
- ~rectangle();
+private:
+    int length;
+    int breadth;
+
+public:
+    // constructors
+    rectangle();
+    rectangle(int l, int b);
+    rectangle(rectangle &r);
+    // mutator
+    int getlength();
+    int getbreadth();
+    // accessor
+    void setlength(int l);
+    void setbreadth(int b);
+    // facilatator
+    int area();
+    int perimeter();
+    // enquary
+    bool isSquare();
+    // destructor
+    ~rectangle();
 };
-int  main()
+int main()
 {
-    
 }
